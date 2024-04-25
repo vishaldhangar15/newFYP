135575b452aa22e617010bbec9001d6589a29450
vishal <vishaldhangar42@gmail.com>
2024-04-25 21:05:22 +0530

    leave section added

--- components/student/LeaveSection/LeaveSection.jsx
+++ components/student/LeaveSection/LeaveSection.jsx
@@ -1,7 +1,129 @@
-import React from 'react';
+import { cn } from '@/lib/utils';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { format } from 'date-fns';
+
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Textarea } from '@/components/ui/textarea';
 
 const LeaveSection = () => {
-  return <div>LeaveSection</div>;
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  console.log(startDate, endDate);
+  return (
+    <div className=" flex flex-col ">
+      <div className={cn('text-center font-extrabold  text-5xl  m-10')}>
+        Going For Leave ?
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="md:m-12">
+          <Card className=" md:w-[450px] w-[370px] ">
+            <CardHeader>
+              <CardTitle>Leave</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">From</Label>
+                    {/* calender  */}
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full justify-start text-left font-normal',
+                            !startDate && 'text-muted-foreground'
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {startDate ? (
+                            format(startDate, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={startDate}
+                          onSelect={setStartDate}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    {/* calender  */}
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <label>Complaint Description</label>
+                    {/* calender  */}
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={'outline'}
+                          className={cn(
+                            'w-full justify-start text-left font-normal',
+                            !endDate && 'text-muted-foreground'
+                          )}
+                        >
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                          {endDate ? (
+                            format(endDate, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0" align="start">
+                        <Calendar
+                          mode="single"
+                          selected={endDate}
+                          onSelect={setEndDate}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                    {/* calender  */}
+                  </div>
+                  <div>
+                    <label>Reason (optional)</label>
+                    <Textarea></Textarea>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button>Deploy</Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeaveSection;
