7c9dba587044947392f55418c5c1d70ce9a34c88
vishal <vishaldhangar42@gmail.com>
2024-05-02 00:27:12 +0530

    loader added

---
+++ app/loading.js
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="h-screen flex justify-center items-center">
+      ] <div className="loader  "></div>
+    </div>
+  );
+}
